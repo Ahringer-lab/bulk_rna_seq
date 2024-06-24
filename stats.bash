b0cd56b46357b43690f2c1c7cf712078fba13c0e
Stewalsh04 <SW2154@cam.ac.uk>
2024-06-24 14:38:33 +0100

    Adding arguments to stats.bash

--- stats.bash
+++ stats.bash
@@ -1,9 +1,39 @@
 #!/bin/bash
 
-analysis_out_dir = $1
-RUNID = $2
+# Function to handle incorrect arguments
+function exit_with_bad_args {
+    echo "Usage: bash lane_merger.bash optional args: --input <input path> --id <Run ID>"
+    echo "Invalid arguments provided" >&2
+    exit # this stops the terminal closing when run as source
+}
 
-cd ${analysis_out_dir}/stats
+INPUT=~/out
+RUNID=RUNID="Stats_summary-$(date '+%Y-%m-%d-%R')"
+
+#Set the possible input options
+options=$(getopt -o '' -l input: -l id: -- "$@") || exit_with_bad_args
+
+#Get the inputs
+eval set -- "$options"
+while true; do
+    case "$1" in
+        --input)
+            shift
+            INPUT="$1"
+            ;;
+        --id)
+            shift
+            RUNID="$1"
+            ;;
+         --)
+            shift
+            break
+            ;;
+    esac
+    shift
+done
+
+cd ${INPUT}/stats
 echo \#Run ID\: ${RUNID} > summary_stats.txt
 echo -n Sample_name, >> summary_stats.txt
 awk 'FNR==3{printf $0 >> "summary_stats.txt"}' *
